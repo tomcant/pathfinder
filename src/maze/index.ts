d86a380e76ab550cbab38d2e1d61d3e458374ebc
Tom Cant <tomcant@gmail.com>
2021-07-13 07:08:04 +0100

    Make getAdjacentPositions() and friends more functional

--- src/maze/index.ts
+++ src/maze/index.ts
@@ -8,25 +8,17 @@ const DIRECTIONS = [
   { dx: 0, dy: -1 },
 ];
 
-const getAdjacentPositions = (maze: Maze, pos: Vec2d, dist: number = 1): Vec2d[] => {
-  const positions = [];
+const getAdjacentPositions = (pos: Vec2d, dist: number): Vec2d[] =>
+  DIRECTIONS.map(({ dx, dy }) => pos.add(new Vec2d(dx * dist, dy * dist)));
 
-  for (const { dx, dy } of DIRECTIONS) {
-    const adjPos = pos.add(new Vec2d(dx * dist, dy * dist));
-
-    if (maze.isWithinBounds(adjPos)) {
-      positions.push(adjPos);
-    }
-  }
-
-  return positions;
-};
+const getAdjacentMazePositions = (maze: Maze, pos: Vec2d, dist: number): Vec2d[] =>
+  getAdjacentPositions(pos, dist).filter((pos) => maze.isWithinBounds(pos));
 
 export const getAdjacentWallPositions = (maze: Maze, pos: Vec2d, dist: number = 1): Vec2d[] =>
-  getAdjacentPositions(maze, pos, dist).filter((pos) => maze.isWall(pos));
+  getAdjacentMazePositions(maze, pos, dist).filter((pos) => maze.isWall(pos));
 
 export const getAdjacentPathPositions = (maze: Maze, pos: Vec2d, dist: number = 1): Vec2d[] =>
-  getAdjacentPositions(maze, pos, dist).filter((pos) => !maze.isWall(pos));
+  getAdjacentMazePositions(maze, pos, dist).filter((pos) => !maze.isWall(pos));
 
 export const findEmptySquareInBounds = (maze: Maze, from: Vec2d, to: Vec2d): Vec2d => {
   let square;
