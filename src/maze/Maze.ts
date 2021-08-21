728134d68dc49b54919b714809425b738897955c
Tom Cant <tomcant@gmail.com>
2021-08-21 12:25:14 +0100

    Add weighted nodes to the Maze implementation

--- src/maze/Maze.ts
+++ src/maze/Maze.ts
@@ -2,6 +2,7 @@ import Vec2d from "../utils/Vec2d";
 
 export default class Maze {
   private walls = new Set<string>();
+  private weights = new Map<string, number>();
 
   private constructor(readonly numCols: number, readonly numRows: number) {}
 
@@ -26,8 +27,7 @@ export default class Maze {
   }
 
   public toggleWalls(positions: Vec2d[]): Maze {
-    const maze = Maze.empty(this.numCols, this.numRows);
-    maze.walls = new Set([...this.walls]);
+    const maze = this.clone();
 
     for (const pos of positions) {
       if (!this.isWithinBounds(pos)) {
@@ -48,7 +48,37 @@ export default class Maze {
     return this.walls.has(pos.toString());
   }
 
+  public setWeight(pos: Vec2d, weight: number): Maze {
+    const maze = this.clone();
+    maze.walls.delete(pos.toString());
+
+    if (weight > 1) {
+      maze.weights.set(pos.toString(), weight);
+    } else {
+      maze.weights.delete(pos.toString());
+    }
+
+    return maze;
+  }
+
+  public getWeight(pos: Vec2d): number {
+    return this.weights.get(pos.toString()) || 1;
+  }
+
+  public isWeight(pos: Vec2d): boolean {
+    return this.weights.has(pos.toString());
+  }
+
   public isWithinBounds(pos: Vec2d): boolean {
     return pos.x >= 0 && pos.y >= 0 && pos.x < this.numCols && pos.y < this.numRows;
+  }
+
+  private clone(): Maze {
+    const maze = Maze.empty(this.numCols, this.numRows);
+
+    maze.walls = new Set([...this.walls]);
+    maze.weights = new Map([...this.weights]);
+
+    return maze;
   }
 }
