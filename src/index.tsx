2858a04e2c14ce851547b6bdbc3b03910d275055
Tom Cant <tomcant@gmail.com>
2022-02-20 14:57:05 +0000

    Hide maze generation features in embed mode for now

--- src/index.tsx
+++ src/index.tsx
@@ -3,29 +3,12 @@ import ReactDOM from "react-dom";
 import PathFinder from "./Components/PathFinder";
 import "./index.css";
 
-const getSizeFromUrl = () => {
-  const size = {
-    cols: null,
-    rows: null,
-  };
-
-  const params = new URLSearchParams(window.location.search);
-
-  // @ts-ignore
-  if (params.has("cols")) size.cols = parseInt(params.get("cols"));
-  // @ts-ignore
-  if (params.has("rows")) size.rows = parseInt(params.get("rows"));
-
-  return size;
-};
-
 const buildPathFinderProps = () => {
   const mazeSize = getSizeFromUrl();
   const squareWidth = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--square-width"));
 
   if (mazeSize.cols === null) {
-    // @ts-ignore
-    const bodyPadding = parseInt(getComputedStyle(document.body).getPropertyValue("padding-left"));
+    const bodyPadding = isEmbedMode() ? 0 : parseInt(getComputedStyle(document.body).getPropertyValue("padding-left"));
     // @ts-ignore
     mazeSize.cols = Math.floor((window.innerWidth - 2 * bodyPadding) / squareWidth);
   }
@@ -46,6 +29,24 @@ const buildPathFinderProps = () => {
   };
 };
 
+const getSizeFromUrl = () => {
+  const size = {
+    cols: null,
+    rows: null,
+  };
+
+  const params = new URLSearchParams(window.location.search);
+
+  // @ts-ignore
+  if (params.has("cols")) size.cols = parseInt(params.get("cols"));
+  // @ts-ignore
+  if (params.has("rows")) size.rows = parseInt(params.get("rows"));
+
+  return size;
+};
+
+const isEmbedMode = () => window.location.search.indexOf("embed") !== -1;
+
 ReactDOM.render(
   <React.StrictMode>
     {/*
@@ -55,9 +56,10 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-if (window.location.search.indexOf("embed") !== -1) {
-  const embedCss =
-    ":root{--nc-tx-1:#000000;--nc-tx-2:#1A1A1A;--nc-bg-1:#FFFFFF;--nc-bg-2:#F6F8FA;--nc-bg-3:#E5E7EB;--nc-lk-1:#0070F3;--nc-lk-2:#0366D6;--nc-lk-tx:#FFFFFF;--nc-ac-1:#79FFE1;--nc-ac-tx:#0C4047;}body{padding:0;}header{display:none;}fieldset#maze-generator{display:none;}";
+if (isEmbedMode()) {
+  const forceLightScheme = ":root{--nc-tx-1:#000000;--nc-tx-2:#1A1A1A;--nc-bg-1:#FFFFFF;--nc-bg-2:#F6F8FA;--nc-bg-3:#E5E7EB;--nc-lk-1:#0070F3;--nc-lk-2:#0366D6;--nc-lk-tx:#FFFFFF;--nc-ac-1:#79FFE1;--nc-ac-tx:#0C4047;}";
+  const overrideFeatures = "body{padding:0;}header{display:none;}fieldset#maze-generator,fieldset#search-method legend,fieldset#search-method select{display:none;}fieldset#search-method{padding:0;border:none;}";
+  const embedCss = forceLightScheme + overrideFeatures;
   const style = document.createElement("style");
   style.appendChild(document.createTextNode(embedCss));
   (document.head || document.getElementsByTagName("head")[0]).appendChild(style);
